c24da354fc2c75c7e2cb60cd3b1c5fa503df05db
Rabi Shiwakoti <46970640+XDevRabi@users.noreply.github.com>
2025-07-25 20:48:20 +0545

    feat(worker): add file processing worker for PDF embedding

---
+++ server/worker.js
@@ -0,0 +1,50 @@
+import { Worker } from 'bullmq';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { QdrantVectorStore } from '@langchain/qdrant';
+import { Document } from '@langchain/core/documents';
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { CharacterTextSplitter } from '@langchain/textsplitters';
+
+const worker = new Worker(
+  'file-upload-queue',
+  async (job) => {
+    console.log(`Job:`, job.data);
+    const data = JSON.parse(job.data);
+    /*
+    Path: data.path
+    read the pdf from path,
+    chunk the pdf,
+    call the openai embedding model for every chunk,
+    store the chunk in qdrant db
+    */
+
+    // Load the PDF
+    const loader = new PDFLoader(data.path);
+    const docs = await loader.load();
+
+    // Embed the docs in vector.
+    const embeddings = new OpenAIEmbeddings({
+      model: 'text-embedding-3-small',
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const vectorStore = await QdrantVectorStore.fromExistingCollection(
+      embeddings,
+      {
+        url: 'http://localhost:6333',
+        collectionName: 'langchainjs-testing',
+      }
+    );
+
+    // Store the vector data in qdrant db.
+    await vectorStore.addDocuments(docs);
+    console.log(`All docs are added to vector store`);
+  },
+  {
+    concurrency: 100,
+    connection: {
+      host: 'localhost',
+      port: '6379',
+    },
+  }
+);
