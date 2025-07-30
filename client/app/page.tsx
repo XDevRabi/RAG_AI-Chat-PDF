f62769e6a1e5f303a1df119618885f4d27c8345c
Rabi Shiwakoti <46970640+XDevRabi@users.noreply.github.com>
2025-07-30 14:31:19 +0545

    refactor(layout): improve page layout structure and responsiveness

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,17 @@
 import FileUploadComponent from './components/file-upload';
 import ChatComponent from './components/chat';
+
 export default function Home() {
   return (
-    <div>
-      <div className="min-h-screen w-screen flex">
-        <div className="w-[30vw] min-h-screen p-4 flex justify-center items-center">
-          <FileUploadComponent />
-        </div>
-        <div className="w-[70vw] min-h-screen border-l-2">
+    <div className="flex flex-col md:flex-row h-[calc(100vh-84px)] w-screen">
+      {/* Sidebar - File Upload */}
+      <div className="w-full md:w-[30vw] p-6 bg-gray-100 flex justify-center items-center border-b-2 md:border-b-0 md:border-r-2">
+        <FileUploadComponent />
+      </div>
+
+      {/* Main Content - Chat */}
+      <div className="w-full md:w-[70vw] flex flex-col">
+        <div className="flex-grow overflow-y-auto bg-white">
           <ChatComponent />
         </div>
       </div>
